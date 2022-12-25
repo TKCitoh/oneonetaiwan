77fb0c00ec8b3b8cd9098ec3f61f4ed37736245d
TKCitoh <tkci10.pooh@gmail.com>
2022-12-25 14:35:00 +0000

    activestorage

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Oneonetaiwan
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    #言語設定の変更
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
---
+++ db/migrate/20221217075335_create_maps.rb
@@ -0,0 +1,11 @@
+class CreateMaps < ActiveRecord::Migration[6.1]
+  def change
+    create_table :maps do |t|
+      t.integer :post_id,   null: false
+      t.float   :latitude,  null: false
+      t.float   :longitude, null: false
+
+      t.timestamps
+    end
+  end
+end
