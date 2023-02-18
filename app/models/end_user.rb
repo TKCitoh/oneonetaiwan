970307aadabd6142a279b8276b4b2030ee7eddd8
oneonetaiwan <tkci10.pooh@gmail.com>
2023-02-18 02:29:21 +0000

    [fix]Confirmable削除

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -16,7 +16,6 @@ class EndUser < ApplicationRecord
   def self.guest
     find_or_create_by!(email: "ttt@ttt.com") do |end_user|
       end_user.password = SecureRandom.urlsafe_base64
-      end_user.password_confirmation = end_user.password
       end_user.name = "ゲスト"
     end
   end
